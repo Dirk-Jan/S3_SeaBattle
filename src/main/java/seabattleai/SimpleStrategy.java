c1a89c12f78ba9db0e9e6508a94be9a304138753
Daphne van de Laar <daphnevandelaar@live.nl>
2018-03-14 15:22:53 +0100

    simple strategy

--- src/main/java/seabattleai/SimpleStrategy.java
+++ src/main/java/seabattleai/SimpleStrategy.java
@@ -1,5 +1,31 @@
 package seabattleai;
 
-public class SimpleStrategy {
+import domain.Field;
+import domain.Square;
+
+import java.util.Random;
+
+public class SimpleStrategy implements IStrategy {
+    private Random random;
+
+    public SimpleStrategy() {
+        random = new Random();
+    }
+
+
+    public Field fireShot(Square square) {
+        Field position = new Field();
+//        SquareState state;
+//        do {
+//            int x = random.nextInt(square.getPosX());
+//            int y = random.nextInt(square.getPosY());
+//            position = square.getPosition(x,y);
+//            state = position.getState();
+//        } while (state == SquareState.SHOTHIT ||
+//                state == SquareState.SHOTMISSED ||
+//                state == SquareState.SHIPSUNK);
+
+        return position;
+    }
 
 }
