a7a8b7a0c9e09f4d207fd4dd0d6fff20d31e5c08
Daphne van de Laar <daphnevandelaar@live.nl>
2018-03-14 13:02:14 +0100

    player en seabattlegame van dj

--- src/main/java/domain/player.java
+++ src/main/java/domain/player.java
@@ -1,11 +1,90 @@
 package domain;
 
 
+import seabattlegui.ShipType;
+import seabattlegui.ShotType;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class player {
+    public void fireShot(){
+
+    private int playerNr;
+    private List<ship> ships;
+
+    private boolean readyToStart = false;
+
+    public boolean isReadyToStart() {
+        return readyToStart;
+    }
+
+    public void setReadyToStart(boolean readyToStart) {
+        this.readyToStart = readyToStart;
+    }
+
+    public List<ship> getShips() {
+        return ships;
+    }
+
+    public int getPlayerNr() {
+        return playerNr;
+    }
+
+    public player(int playerNr) {
+        this.playerNr = playerNr;
+        this.ships = fillInventoryWithShips();
+    }
+
+    private List<ship> fillInventoryWithShips(){
+        List<ship> ships = new ArrayList<ship>();
+        // TODO add ships
+        return ships;
+    }
+
+    public void placeShipsAutomatically() {
+        int randomX, randomY;
+        Random randomGenerator = new Random();
+
+        for(ship ship : ships) {
+            randomX = getRandomNumber(randomGenerator, 0, 9);
+            randomY = getRandomNumber(randomGenerator, 0, 9);
+
+            while(canShipBePlaced(ship, randomX, randomY, true)){
+                randomX = getRandomNumber(randomGenerator, 0, 9);
+                randomY = getRandomNumber(randomGenerator, 0, 9);
+            }
+
+            placeShipOnField(ship, randomX, randomY, true);
+        }
+    }
+
+    private int getRandomNumber(Random random, int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public ShotType fireShot(int posX, int posY){
+
+        return null;
+    }
+
+    public boolean placeShip(ShipType shipType, int bowX, int bowY, boolean horizontal){
+        ship shipToPlace = null;
+        for(ship ship : ships) {
+            if(ShipType.AIRCRAFTCARRIER == shipType){   // TODO change shipType.Ari.. to ship.getType()
+                shipToPlace = ship;
+            }
+        }
+
+        if(shipToPlace == null){
+            return false;
+        }
+
+        if(canShipBePlaced(shipToPlace, bowX, bowY, horizontal)) {
+            removeShipFromInventory(shipToPlace);
+            placeShipOnField(shipToPlace, bowX, bowY, horizontal);
 
     public void fireShot(){
 
@@ -14,19 +93,32 @@ public class player {
     public boolean placeShip(){
        return true;
     }
-    public void removeShip(){
 
+    private void placeShipOnField(ship ship, int bowX, int bowY, boolean horizontal) {
+        // TODO add ahip to field (FIELD required)
     }
 
-    public List<ship> getShipsFromInventory(){
-        throw new NotImplementedException();
+    private boolean canShipBePlaced(ship shipToPlace, int bowX, int bowY, boolean horizontal) {
+        // TODO Implement canshipbeplaced algorithm (Field required)
     }
 
-    public void addShipToInventory(ship ship){
 
+    public boolean removeShip(int posX, int posY){
+        // TODO Implement remove ship (Field required)
+
+        return false;
     }
 
     public void removeShipFromInventory(ship ship){
+        ships.remove(ship);
+    }
 
+    public boolean removeAllShips() {
+        // TODO Implement remove all ships (Field class required)
+        return true;
+    }
+
+    public ShotType receiveShot(int posX, int posY) {
+        // TODO Implement receiveShot
     }
 }
--- src/main/java/seabattlegame/SeaBattleGame.java
+++ src/main/java/seabattlegame/SeaBattleGame.java
@@ -5,9 +5,15 @@
  */
 package seabattlegame;
 
+import domain.player;
+import domain.ship;
 import seabattlegui.ISeaBattleGUI;
+import seabattlegui.SeaBattleApplication;
 import seabattlegui.ShipType;
 import seabattlegui.ShotType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The Sea Battle game. To be implemented.
@@ -15,48 +21,95 @@ import seabattlegui.ShotType;
  */
 public class SeaBattleGame implements ISeaBattleGame {
 
+    private List<player> players = new ArrayList<player>();
+
+    private player getPlayerByNumber(int playerNr) {
+        for(player player : players){
+            if(player.getPlayerNr() == playerNr){
+                return player;
+            }
+        }
+        return null;
+    }
+
     @Override
     public int registerPlayer(String name, ISeaBattleGUI application, boolean singlePlayerMode) {
-        throw new UnsupportedOperationException("Method registerPlayer() not implemented.");
+        player player = new player(players.size());
+        players.add(player);
+        return player.getPlayerNr();
     }
 
     @Override
     public boolean placeShipsAutomatically(int playerNr) {
-        throw new UnsupportedOperationException("Method placeShipsAutomatically not implemented.");
+        for(player player : players){
+            if(player.getPlayerNr() == playerNr) {
+                player.placeShipsAutomatically();
+            }
+        }
     }
 
     @Override
     public boolean placeShip(int playerNr, ShipType shipType, int bowX, int bowY, boolean horizontal) {
-        throw new UnsupportedOperationException("Method placeShip() not implemented.");
+        player player = getPlayerByNumber(playerNr);
+        if(player != null) {
+            return player.placeShip(shipType, bowX, bowY, horizontal);
+        }
+        return false;
     }
 
     @Override
     public boolean removeShip(int playerNr, int posX, int posY) {
-        throw new UnsupportedOperationException("Method removeShip() not implemented.");
+        player player = getPlayerByNumber(playerNr);
+        if(player != null) {
+            return player.removeShip(posX, posY);
+        }
+        return false;
     }
 
     @Override
     public boolean removeAllShips(int playerNr) {
-        throw new UnsupportedOperationException("Method removeAllShips() not implemented.");
+        player player = getPlayerByNumber(playerNr);
+        if(player != null) {
+            return player.removeAllShips();
+        }
+        return false;
     }
 
     @Override
     public boolean notifyWhenReady(int playerNr) {
-        throw new UnsupportedOperationException("Method notifyWhenReady() not implemented.");
+        player player = getPlayerByNumber(playerNr);
+        if(player != null) {
+            player.setReadyToStart(true);
+        }
     }
 
     @Override
     public ShotType fireShotPlayer(int playerNr, int posX, int posY) {
-        throw new UnsupportedOperationException("Method fireShotPlayer() not implemented.");
+        player firingPlayer = getPlayerByNumber(playerNr);
+        if(firingPlayer != null) {
+            firingPlayer.fireShot(posX, posY);
+
+            player receivingPlayer = getPlayerByNumber(playerNr == 0 ? 1 : 0);
+            if(receivingPlayer != null) {
+                return receivingPlayer.receiveShot(posX, posY);
+            }
+        }
+        return ShotType.MISSED;
     }
 
     @Override
-    public ShotType fireShotOpponent(int playerNr) {
-        throw new UnsupportedOperationException("Method fireShotOpponent() not implemented.");
+    public ShotType fireShotOpponent(int playerNr) {    // Only used in singleplayer, playerNr is the human player
+        player player = getPlayerByNumber(playerNr);
+        if(player != null) {
+            // TODO use strategy here and fire shot at player
+            return player.receiveShot(0,0);
+        }
+        return ShotType.MISSED;
     }
 
     @Override
     public boolean startNewGame(int playerNr) {
-        throw new UnsupportedOperationException("Method startNewGame() not implemented.");
+        players = new ArrayList<player>();
+        // TODO Probably need to close and reopen some GUIs too
     }
 }
