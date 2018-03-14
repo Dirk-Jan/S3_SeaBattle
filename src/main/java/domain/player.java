cd06089fd0524168a06371438f1913cb464e92df
Daphne van de Laar <daphnevandelaar@live.nl>
2018-03-14 08:46:40 +0100

    classes
    d

---
+++ src/main/java/domain/player.java
@@ -0,0 +1,30 @@
+package domain;
+
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+public class player {
+    public void fireShot(){
+
+    }
+    public boolean placeShip(){
+       return true;
+    }
+    public void removeShip(){
+
+    }
+
+    public List<ship> getShipsFromInventory(){
+        throw new NotImplementedException();
+    }
+
+    public void addShipToInventory(ship ship){
+
+    }
+
+    public void removeShipFromInventory(ship ship){
+
+    }
+}
---
+++ src/main/java/domain/ship.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class ship {
+    private int length;
+    private int location;
+
+
+    public void sink(){
+
+    }
+}
