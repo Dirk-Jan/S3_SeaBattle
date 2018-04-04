209b02d99e113aa35d45ff3c955fee09ad7846b9
Daphne van de Laar <daphnevandelaar@live.nl>
2018-04-04 12:03:58 +0200

    message handler toegevoegd en test aangepast

---
+++ src/main/java/seabattlegame/websocket/client/MessageHandler.java
@@ -0,0 +1,22 @@
+package seabattlegame.websocket.client;
+
+import seabattlegui.ISeaBattleGUI;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MessageHandler implements Observer{
+
+    ISeaBattleGUI gui;
+
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+        switch(){
+            case
+        }
+    }
+
+
+}
