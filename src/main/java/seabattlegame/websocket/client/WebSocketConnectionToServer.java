0d4b43db9dbd62b6f772bff07d42e89be4170ae6
Daphne van de Laar <daphnevandelaar@live.nl>
2018-04-04 12:05:04 +0200

    websocket connection

--- src/main/java/seabattlegame/websocket/client/WebSocketConnectionToServer.java
+++ src/main/java/seabattlegame/websocket/client/WebSocketConnectionToServer.java
@@ -1,7 +1,48 @@
 package seabattlegame.websocket.client;
 
-public class WebSocketConnectionToServer {
+import org.eclipse.jetty.util.component.LifeCycle;
+
+import javax.websocket.ContainerProvider;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+import java.net.URI;
+import java.util.Observable;
+
+public class WebSocketConnectionToServer extends Observable {
 
     public WebSocketConnectionToServer() {
+
+    }
+
+    private Session session;
+
+    public void createConnection(){
+        URI uri = URI.create("ws://localhost:8010/seabattlegame/");
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            try {
+                // Attempt Connect
+                session = container.connectToServer(EventClientSocket.class, uri);
+                // Send a message
+                session.getBasicRemote().sendText("Hello");
+                // Close session
+//                    Thread.sleep(10000);
+//                    session.close();
+            } finally {
+                // Force lifecycle stop when done with container.
+                // This is to free up threads and resources that the
+                // JSR-356 container allocates. But unfortunately
+                // the JSR-356 spec does not handle lifecycles (yet)
+                if (container instanceof LifeCycle) {
+                    ((LifeCycle) container).stop();
+                }
+            }
+        } catch (Throwable t) {
+            t.printStackTrace(System.err);
+        }
+    }
+
+    public void closeConnection(){
+
     }
 }
